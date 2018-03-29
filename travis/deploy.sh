1ac2c9364c4881bf867370b7f445804bc2d67be8
rogerparkinson <roger.parkinson35@gmail.com>
2018-03-29 21:14:25 +1300

    travis

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ]; then
+if [ ! -z "$TRAVIS_TAG" ]; then
 	echo "deploying tag: $TRAVIS_TAG"
     mvn versions:set -DnewVersion=${TRAVIS_TAG}
     mvn --settings travis/settings.xml clean deploy -B -U -P release
